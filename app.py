2f7b2297600da510027c0a4edc1da2bd288a860a
Reinny <reynsth@gmail.com>
2020-07-16 22:42:58 -0400

    :arrow_up: Added Frozen-Flask

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route("/")
+@app.route("/home.html")
 def home():
     return render_template("home.html")
 
-@app.route("/index")
+@app.route("/index.html")
 def index():
     return render_template("index.html")
 
-@app.route("/fox/<fox_name>")
+@app.route("/fox/<fox_name>.html")
 def fox(fox_name):
    return render_template(f"foxes/{fox_name}.html")
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run()
+    freezer.freeze()
