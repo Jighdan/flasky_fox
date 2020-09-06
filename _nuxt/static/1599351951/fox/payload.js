8b28d16879f3c59a7d414c6f3a5748a9a402c455
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-06 00:26:04 +0000

    Deploy Jighdan/flasky_fox to github.com/Jighdan/flasky_fox.git:gh-pages

---
+++ _nuxt/static/1599351951/fox/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/fox", (function(a,b,c,d,e,f){return {data:[{allFoxes:[{name:"Arabian Red",slug:"ArabianRed"},{name:a,slug:a},{name:b,slug:b},{name:c,slug:c},{name:d,slug:d},{name:e,slug:e},{name:f,slug:f},{name:"Tibetan Sand",slug:"TibetanSand"}]}],fetch:[],mutations:void 0}}("Artic","Fennec","Gray","Kit","Red","Swift")));
